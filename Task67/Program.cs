77b5a351308fb1f0882af40e8863302e23be9773
«Artur» <art13471118@gmail.com>
2022-12-30 10:41:00 +0300

    Рекурсия.Сумма цифр вводимого чисал

---
+++ Task67/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("M");
+int M = int.Parse(Console.ReadLine()!);
+
+int Sum(int A)
+{
+    int sum = 0;
+    while(A!=0)
+    {
+        sum+=A%10;
+        A=A/10;
+    }
+    return sum;
+}
+
+int Rec(int A)
+{
+    if(A==0) return 0;
+    else return A%10 + Rec(A/10);
+}
+
+
+
+Console.WriteLine(Sum(M));
+Console.WriteLine(Rec(M));
